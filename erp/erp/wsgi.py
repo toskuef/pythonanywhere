2cdae531a2a1e9d77d5da8a343149a9444100b2a
toskuef <96075802+toskuef@users.noreply.github.com>
2022-06-09 23:54:13 +0300

    vk

--- erp/erp/wsgi.py
+++ erp/erp/wsgi.py
@@ -7,10 +7,25 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+# import os
+#
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erp.settings')
+#
+# application = get_wsgi_application()
+
 import os
+import signal
 
+import sys
+import traceback
+
+import time
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "erp.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
